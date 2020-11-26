d058c1c5b8bcf01d00d97ae16dd51f001b5ef8e7
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-26 21:57:33 +0530

    Added:Logic to check last name with caps and has minimum 3 characters

--- registration.sh
+++ registration.sh
@@ -1,9 +1,21 @@
 echo "Welcome of the User Registration program being solved in the Master Branch"
 read -p "enter your first name: " firstname
 firstnameRegex="^[A-Z]{1}[a-z]{2,}"
+
 if(($firstname =~ $firstnameRegex))
 then
       echo "$firstname is a valid name"
 else
      echo "invalid name"
 fi
+echo "---------------------------------------"
+read -p "enter your last name: " lastname
+lastnameRegex="^[A-Z]{1}[a-z]{2,}"
+
+if(($lastname =~ $lastnameRegex))
+then
+      echo "$lastname is a valid name"
+else
+     echo "invalid name"
+fi
+
