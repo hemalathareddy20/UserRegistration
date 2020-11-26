7c4381e4211356eb193e2d3a8b7fa241a4646e14
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-26 22:20:57 +0530

    Added:Logic to check valid password or not

--- registration.sh
+++ registration.sh
@@ -1,8 +1,7 @@
 echo "Welcome of the User Registration program being solved in the Master Branch"
 read -p "enter your first name: " firstname
 firstnameRegex="^[A-Z]{1}[a-z]{2,}"
-
-if(($firstname =~ $firstnameRegex))
+if(( $firstname =~ $firstnameRegex ))
 then
       echo "$firstname is a valid name"
 else
@@ -11,8 +10,7 @@ fi
 echo "---------------------------------------"
 read -p "enter your last name: " lastname
 lastnameRegex="^[A-Z]{1}[a-z]{2,}"
-
-if(($lastname =~ $lastnameRegex))
+if(( $lastname =~ $lastnameRegex ))
 then
       echo "$lastname is a valid name"
 else
@@ -21,7 +19,7 @@ fi
 echo "---------------------------------------"
 read -p "enter a valid email Id: " emailID
 emailpat="^[a-zA-Z0-9\-\+\.]*.([a-zA-Z0-9])?@([a-z0-9]*.[a-z]{2,4}.([a-z]{2,})?)$"
-if(($emailID =~ $emailpat))
+if(( $emailID =~ $emailpat ))
 then
      echo "$emailID is a valid email"
 else
@@ -30,9 +28,19 @@ fi
 echo "----------------------------------------"
 read -p "enter a valid mobile number: " mobilenumber
 pat="[0-9]{2}[ ]?[0-9]{10}$"
-if(($mobilenumber =~ $pat))
+if(( $mobilenumber =~ $pat ))
 then
      echo "$mobilenumber is a valid mobile number"
 else
      echo "invalid mobile number"
 fi
+echo "------------------------------------------"
+read -p "enter a valid password(atleast 8 characters,1 number and 1 uppercase and exactly 1 special character) : " password
+pat1="^(?=.{8,}$)(?=.*\d)(?=.*[A-Z])[a-zA-Z0-9]*[\@\#\^\!]*[a-zA-z0-9]*$"
+if(( $password =~ $pat1 ))
+then
+     echo "$mobilenumber is a valid password"
+else
+     echo "invalid password"
+fi
+
