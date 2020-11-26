df40ca5bd74d1b70088c473886870b69bc3e4e67
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-26 22:02:55 +0530

    Added:Logic to check valid email or not

--- registration.sh
+++ registration.sh
@@ -18,4 +18,12 @@ then
 else
      echo "invalid name"
 fi
-
+echo "---------------------------------------"
+read -p "enter a valid email Id: " emailID
+emailpat="^[a-zA-Z0-9\-\+\.]*.([a-zA-Z0-9])?@([a-z0-9]*.[a-z]{2,4}.([a-z]{2,})?)$"
+if(($emailID =~ $emailpat))
+then
+     echo "$emailID is a valid email"
+else
+     echo "invalid email"
+fi
